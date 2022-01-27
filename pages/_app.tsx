b445c7a66658b0b2405fecf16e6528788582da05
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2022-01-27 02:42:55 +0200

    feat: add splitbee analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Script src="https://cdn.splitbee.io/sb.js" />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
