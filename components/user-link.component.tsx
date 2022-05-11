ed85c2ae38d67c83544cc675189d45938d92394c
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2022-05-11 22:10:46 +0300

    refactor: adjust users data

--- components/user-link.component.tsx
+++ components/user-link.component.tsx
@@ -2,32 +2,22 @@ import usersInfo from "../data/users-info.json";
 
 export function UserLink({ user }: { user: string }) {
 	const lowerCaseUser = user.toLowerCase();
-	const userInfo = usersInfo.find(
-		(info) => info.name.toLowerCase() === lowerCaseUser,
-	);
+	const userInfo = usersInfo.find(({ name }) => {
+		return name.toLowerCase() === lowerCaseUser;
+	});
 
-	if (!userInfo) return <span>{lowerCaseUser}</span>;
+	const social = userInfo?.socials[0];
 
-	if (userInfo.social)
-		return (
-			<a
-				href={userInfo.social.url}
-				target="_blank"
-				rel="noopener noreferrer"
-				className="hover:underline"
-			>
-				{userInfo.social.handle}
-			</a>
-		);
+	if (!social) return <span>{lowerCaseUser}</span>;
 
 	return (
 		<a
-			href={`https://topicmd.com/userdetails.php?id=${userInfo.tmdId}`}
+			href={social.url}
 			target="_blank"
 			rel="noopener noreferrer"
 			className="hover:underline"
 		>
-			{userInfo.name}
+			{social.handle}
 		</a>
 	);
 }
--- utils/index.ts
+++ utils/index.ts
@@ -6,6 +6,7 @@ export function getPiecesInfo(filter: (piece: string) => boolean) {
 		const path = piece.replace("./public", "");
 		const [_, _pieces, info, user, _ext] = path
 			.replace(".", "/")
+			// replaces only first underscore, some users have udnerscore in name
 			.replace("_", "/")
 			.split("/");
 		return { path, user, info };
